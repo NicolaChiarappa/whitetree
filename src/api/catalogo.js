cba7939b9190f24573b22fc1662d41e4ee6bfa69
Nick <nicolachiarappa2001@gmail.com>
2023-08-09 18:04:09 +0200

    errata corrige printful get

--- src/api/catalogo.js
+++ src/api/catalogo.js
@@ -2,6 +2,6 @@ import { PrintfulClient } from "printful-request";
 
 const printful = new PrintfulClient("WE1TCUYTjuG9CbhoiSjmDVEePxkgvQl8SzYdweKj");
 function funzione() {
-  printful.get("products").then(({ result }) => console.log(result));
+  printful.get("store/products").then(({ result }) => console.log(result));
 }
 export default funzione;
