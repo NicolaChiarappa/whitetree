e85623bd4aa170fdc4c3c7332deb38cbcf82ea66
Nick <nicolachiarappa2001@gmail.com>
2023-09-01 21:02:09 +0200

    ideatext bug fixed

--- src/pages/idea.js
+++ src/pages/idea.js
@@ -4,6 +4,8 @@ import Navbar from "../components/Navbar";
 import { IoBulbOutline } from "react-icons/io5";
 
 const Idea = () => {
+  const ideaText =
+    'Da qui parte I identità del nostro brand, dal forte romanticismo che ci porta a voler esaltare la quotidianità e le sue profonde emozioni attraverso i disegni unici delle nostre maglie, fatti in bianco e nero, per lasciare a colui che le indossa la possibilità di "colorarle" con le emozioni che questa può provocare. Ogni disegno ha una storia, vissuta da tutti ma per tutti in qualche modo diversa, scene già viste ma questa volta lette attraverso i sonetti da noi composti esclusivamente per i nostri prodotti, affinché possano essere una lente per la vostra personalissima interpretazione dell\'opera.';
   return (
     <>
       <Navbar></Navbar>
@@ -16,17 +18,7 @@ const Idea = () => {
         <h2 className='font-bold   text-2xl text-center  md:text-3xl italic mt-10'>
           Non vi è colore nell'occhio <br></br>se non vi è emozione nel cuore
         </h2>
-        <p className='leading-relaxed mt-10 text-xl text-justify'>
-          Da qui parte I identità del nostro brand, dal forte romanticismo che
-          ci porta a voler esaltare la quotidianità e le sue profonde emozioni
-          attraverso i disegni unici delle nostre maglie, fatti in bianco e
-          nero, per lasciare a colui che le indossa la possibilità di
-          "colorarle" con le emozioni che questa può provocare. Ogni disegno ha
-          una storia, vissuta da tutti ma per tutti in qualche modo diversa,
-          scene già viste ma questa volta lette attraverso i sonetti da noi
-          composti esclusivamente per i nostri prodotti, affinché possano essere
-          una lente per la vostra personalissima interpretazione dell'opera.
-        </p>
+        <p className='leading-relaxed mt-10 text-xl text-justify'>{ideaText}</p>
       </VStack>
     </>
   );
