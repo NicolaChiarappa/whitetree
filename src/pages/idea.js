fba192429f30c46a5a8a895c7c2d556bc93b92da
Nick <nicolachiarappa2001@gmail.com>
2023-09-01 21:11:18 +0200

    quotes bug fixed

--- src/pages/idea.js
+++ src/pages/idea.js
@@ -12,11 +12,13 @@ const Idea = () => {
       <VStack style='text-white  items-center  md:mt-20 mt-10 px-10'>
         <HStack style='items-center text-3xl font-bold '>
           <IoBulbOutline color='white' size={50}></IoBulbOutline>
-          <h1>L'idea</h1>
+          <h1>{"L'idea"}</h1>
         </HStack>
 
         <h2 className='font-bold   text-2xl text-center  md:text-3xl italic mt-10'>
-          Non vi è colore nell'occhio <br></br>se non vi è emozione nel cuore
+          {
+            "Non vi è colore nell'occhio <br></br>se non vi è emozione nel cuore"
+          }
         </h2>
         <p className='leading-relaxed mt-10 text-xl text-justify'>{ideaText}</p>
       </VStack>
