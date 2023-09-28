70b16cd5824d4b39bad09c88eb70e0f3384eeef2
Nick <nicolachiarappa2001@gmail.com>
2023-09-28 20:03:27 +0200

    success page algorithm

--- src/pages/success.js
+++ src/pages/success.js
@@ -1,8 +1,22 @@
-import { NextApiRequest, NextApiResponse } from "next";
+import { getCart } from "@/app/firebase/database";
+import { currentUser } from "@/app/firebase/auth";
+import axios from "axios";
+import { useState } from "react";
 
-const success = (req, res) => {
-  console.log(req);
-  return <div>Ciaooo</div>;
+const Success = () => {
+  const [res, setRes] = useState(null);
+  axios
+    .get("https://nice-pear-dalmatian-garb.cyclic.app/success")
+    .then((res) => {
+      setRes(res);
+    });
+  return res == null ? (
+    <></>
+  ) : res == "good" ? (
+    <div className='text-white'>Good</div>
+  ) : (
+    <div className='text-white'>Not good</div>
+  );
 };
 
-export default success;
+export default Success;
