788685763a57c3db54f3eadf3c299b19018a60d1
Nick <nicolachiarappa2001@gmail.com>
2023-10-02 22:10:36 +0200

    fixed facebook pixel

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -3,13 +3,13 @@ import { Analytics } from "@vercel/analytics/react";
 import ErrorBoundary from "../api/errhandl";
 import TagManager from "react-gtm-module";
 import { useEffect } from "react";
-import ReactPixel from "react-facebook-pixel";
+// import ReactPixel from "react-facebook-pixel";
 export default function App({ Component, pageProps }) {
-  const options = {
-    autoConfig: true, // set pixel's autoConfig. More info: https://developers.facebook.com/docs/facebook-pixel/advanced/
-    debug: false, // enable logs
-  };
   useEffect(() => {
+    const options = {
+      autoConfig: true, // set pixel's autoConfig. More info: https://developers.facebook.com/docs/facebook-pixel/advanced/
+      debug: false, // enable logs
+    };
     ReactPixel.init("1389151521669261", options);
     ReactPixel.pageView();
 
