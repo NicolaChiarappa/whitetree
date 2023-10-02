bdc62f821729760419c0277023e7923bf9dd6479
Nick <nicolachiarappa2001@gmail.com>
2023-10-02 22:03:40 +0200

    facebook pixel added

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -3,8 +3,16 @@ import { Analytics } from "@vercel/analytics/react";
 import ErrorBoundary from "../api/errhandl";
 import TagManager from "react-gtm-module";
 import { useEffect } from "react";
+import ReactPixel from "react-facebook-pixel";
 export default function App({ Component, pageProps }) {
+  const options = {
+    autoConfig: true, // set pixel's autoConfig. More info: https://developers.facebook.com/docs/facebook-pixel/advanced/
+    debug: false, // enable logs
+  };
   useEffect(() => {
+    ReactPixel.init("1389151521669261", options);
+    ReactPixel.pageView();
+
     TagManager.initialize({ gtmId: "G-8PBFCXV3YL" });
   }, []);
   return (
