e3fedebb8a32eaa2be7061430704b641d50840f7
Nick <nicolachiarappa2001@gmail.com>
2023-10-03 02:55:32 +0200

    fixed facebook pixel n.2

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -3,18 +3,27 @@ import { Analytics } from "@vercel/analytics/react";
 import ErrorBoundary from "../api/errhandl";
 import TagManager from "react-gtm-module";
 import { useEffect } from "react";
-// import ReactPixel from "react-facebook-pixel";
-export default function App({ Component, pageProps }) {
-  useEffect(() => {
-    const options = {
-      autoConfig: true, // set pixel's autoConfig. More info: https://developers.facebook.com/docs/facebook-pixel/advanced/
-      debug: false, // enable logs
-    };
-    ReactPixel.init("1389151521669261", options);
-    ReactPixel.pageView();
 
+import { useRouter } from "next/router";
+import Router from "next/router";
+
+export default function App({ Component, pageProps }) {
+  const router = useRouter();
+  useEffect(() => {
     TagManager.initialize({ gtmId: "G-8PBFCXV3YL" });
   }, []);
+  useEffect(() => {
+    import("react-facebook-pixel")
+      .then((x) => x.default)
+      .then((ReactPixel) => {
+        ReactPixel.init("1389151521669261");
+        ReactPixel.pageView();
+
+        router.events.on("routeChangeComplete", () => {
+          ReactPixel.pageView();
+        });
+      });
+  }, [router.events]);
   return (
     <div>
       <ErrorBoundary>
